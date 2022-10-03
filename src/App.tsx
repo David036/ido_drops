6ee3f08382826d0e315af26116f1b9e2d9993241
David036 <hovhannisyandav@icloud.com>
2022-10-03 12:13:27 +0400

    Created empty react-typescript project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/feature/header';
+
+import './App.css';
+import './styles/global.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
